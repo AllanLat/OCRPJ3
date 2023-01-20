5c0626cd04b3448449e69744cc70de10eb214bb3
FullStackMan <allan.latruffe@gmail.com>
2023-01-20 19:58:11 +0100

    add init project, categori, call Api and addEvent

--- FrontEnd/project.js
+++ FrontEnd/project.js
@@ -1,14 +1,32 @@
-// Views project.
+// List des import.
 
+// Variables global
+let projectsList;
+let categories;
 
-async function viewsProject(){
+async function callApiProjectsCategories() {
 
-    // GET in const Project.
-    const projects = await fetch("http://localhost:5678/api/works");
-    const worksJson = await projects.json();
+    const projectsCategories =  await fetch("http://localhost:5678/api/works");
+    const projects = await projectsCategories.json();
+
+    const categoriesOnJson = projects.map(itemCats => itemCats.category);
+    
+    projectsList = projects;
+    categories = categoriesOnJson;
+
+    // On appel l'affichage des btn et des project juste ici 
+    viewsCategorie(categories);
+    viewsProjects(projectsList);
+    // END call 
+}
+
+async function viewsProjects(listProject) {
 
     const gallery = document.querySelector(".gallery");
     gallery.innerHTML = ""; 
+
+    const worksJson = listProject;
+
 
     // Create loop
     for(let i = 0; i < worksJson.length; i++) {
@@ -27,8 +45,65 @@ async function viewsProject(){
         gallery.appendChild(figure);
 
     }
-    
 }
 
-// Appeler la function aillant une boucle 
-viewsProject();
+async function viewsCategorie(listCategories) {
+
+    // On récupére le listing des catégories et 
+    // On le trie avec Set de façon à ne resortir que les ids 
+    const catId = new Set(listCategories.map(catRow => catRow.id));
+
+    // Création du button All : 
+    const btnChoiceConteneur = document.querySelector('#btnChoiceConteneur');
+    const buttonDefault = document.createElement('button');
+    buttonDefault.id = "resetButtonCategorie";
+    buttonDefault.innerText = "Tous";
+    btnChoiceConteneur.appendChild(buttonDefault);
+   
+    // On affiche ensuite seulement les btns dont les ids sont présent 
+        // Créé une boucle sur le Set.
+        for(const values of catId) {
+            
+             // Chaque appel on va cherche dans le JSON globale le première element correspondant à l'ID
+            const categoriButtonContent = Object.values(categories);
+            const searchCategoriButton = categoriButtonContent.find(item => item.id === values);
+
+            const buttonCat = document.createElement('button');
+
+            buttonCat.id = searchCategoriButton.id;
+            buttonCat.innerText = searchCategoriButton.name;
+
+            btnChoiceConteneur.appendChild(buttonCat); 
+ 
+        }
+ 
+}
+
+// Appel de la fonction inialisation
+
+callApiProjectsCategories()
+
+// Ajout des event
+
+const groupBtn = document.querySelector('#btnChoiceConteneur');
+
+groupBtn.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+
+        const IdButton = event.target.id;
+
+        if (IdButton > 0) {
+            const listTrier = projectsList.filter(function (projectButton){
+                return projectButton.categoryId == IdButton;
+            });
+            viewsProjects(listTrier);
+        }
+        else {
+            viewsProjects(projectsList);
+        }
+      
+
+        
+    }
+})
+
