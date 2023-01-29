b5a6a776730c8159e4fbee18bef59cf6fc310330
FullStackMan <allan.latruffe@gmail.com>
2023-01-29 13:15:04 +0100

    update systeme url

--- FrontEnd/assets/js/tools.js
+++ FrontEnd/assets/js/tools.js
@@ -1,9 +1,16 @@
 // Je ne sais pas ou placer cette function 
 
 export function validateUrl(page){
-    const url = document.location.pathname;
+    const url = location.pathname;
     const regex = new RegExp(`\\${page}.html$`);
-    const validatedUrl = regex.test(url);
-
+    let validatedUrl = regex.test(url);
+    
+    if (!validatedUrl && page === 'index' ) {
+        if (url.search('.html') === -1) {
+            validatedUrl = true;
+        }
+    }
+    
+    console.log(validatedUrl);
     return validatedUrl;
 }
